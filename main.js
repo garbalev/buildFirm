141a4ea495520c034e17b93ef7a064a95bfb4467
Alexander Garbalev <garbalevalex@mail.ru>
2021-09-12 16:05:40 +0300

    redesign

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 var firstPageScroll = document.querySelector('.firstPageScroll'),
-    sixthPage = document.querySelector('.sixthPage')
+    sixthPage = document.querySelector('.sixthPage'),
+    allHrefs = document.querySelectorAll('.secondPageHeaderNavBar a, .sixthPageLinksFirst a')
 
 firstPageScroll.addEventListener('click', scrollDown);
 
+
+
 function scrollDown() {
-    window.scrollTo(0, sixthPage.offsetTop);
+    window.scrollTo({top: sixthPage.offsetTop, behavior: 'smooth'});
 };
+
+document.addEventListener('click', function(e){
+    e.preventDefault();
+    allHrefs.forEach(function(item){
+        if(item === e.target){
+            var id = e.target.getAttribute('href'),
+            top = document.querySelector(id).offsetTop;
+        window.scrollTo({ top: top, behavior: 'smooth'});
+        }
+    })     
+})
